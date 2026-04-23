a4e34bf8e8d95c47b19f0e9f34ad1868550b1447
ede003 <edenyfernandez003@gmail.com>
2026-04-23 10:59:28 -0400

    tailwind configuration

---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+export default function Home() {
+  return (
+    <View className="flex-1 justify-center items-center">
+  <Text className="bg-blue-500 text-white text-2xl">
+    TEST
+  </Text>
+</View>
+  );
+}
