45195ac16e8f59014b2fa0bfd250ece4dcabf0c3
Edeny Fernandez <edenyfernandez003@gmail.com>
2026-04-24 16:40:42 -0400

    Example fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,11 +4,34 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function Home() {
   return (
     <SafeAreaView>
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-poppins text-primary">Test</Text>
-      <Text className="text-2xl font-inter text-secondary">Test</Text>
-      <Text className="text-2xl font-medium text-tertiary">Test</Text>
-    </View>
+    <View className="px-4 pt-6">
+
+        {/* titulos */}
+        <Text className="text-3xl font-poppins text-black">
+          Explorar comida
+        </Text>
+
+         {/* Subtitulos1 */}
+        <Text className="text-xl font-medium text-primary mt-2">
+          Categorías
+        </Text>
+
+        {/* Subtitulos2 */}
+        <Text className="text-base font-inter text-secondary mt-2">
+          Descubre platos y rutas gastronómicas en tu ciudad
+        </Text>
+
+        {/* texto normal */}
+        <Text className="text-base font-inter text-black mt-2">
+          Elige entre comida típica, rápida o callejera
+        </Text>
+
+        {/* texto pequeño */}
+        <Text className="text-sm font-inter text-tertiary mt-4">
+          Recomendado para ti
+        </Text>
+
+      </View>
     </SafeAreaView>
   );
 }
