254603d9336afc212805835b3e1a0fed81f1ff8b
Edeny Fernandez <edenyfernandez003@gmail.com>
2026-04-30 16:22:34 -0400

    datoshome

---
+++ src/features/menu/data/mockData.ts
@@ -0,0 +1,26 @@
+// Rutas gastronómicas - Mock data
+export const GASTRO_ROUTES = [
+    {
+        id: "1",
+        title: "Ruta de la Salteña – La Paz",
+        duration: "3 horas",
+        rating: "4.9",
+        reviews: "120",
+        badge: "POPULAR",
+        image: require("../../../assets/images/images-routes-home/Ruta-cochabamba.jpg"),
+    },
+];
+
+// Platos en tendencia - Mock data
+export const TRENDING_DISHES = [
+    {
+        id: "1",
+        name: "Silpancho\nCochabambino",
+        image: require("../../../assets/images/images-routes-home/silpancho-home.jpg"),
+    },
+    {
+        id: "2",
+        name: "Especial\nLa Casa del Pique",
+        image: require("../../../assets/images/images-routes-home/pique-home.jpg"),
+    },
+];
