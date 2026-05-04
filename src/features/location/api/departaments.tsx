a0348867438d44ae525d10cfe4a2982a6aba870c
Edeny Fernandez <edenyfernandez003@gmail.com>
2026-05-03 21:41:46 -0400

    feat: CreateDepartmentContext.tsx

--- src/features/location/api/departaments.tsx
+++ src/features/location/api/departaments.tsx
@@ -4,9 +4,6 @@ import { Department } from "../types/Department";
 
 export async function getDepartments() {
   const { data, error } = await supabase.rpc("get_departments");
-  console.log("DATA:", JSON.stringify(data));
-  console.log("ERROR:", JSON.stringify(error));
-  
   if (error) {
     throw error;
   }
